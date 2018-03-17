c55cb8faff58e1a76e6ccb3cf63875f43f6741ea
pavel-borik <pavel.borik1@gmail.com>
2018-03-17 20:40:02 +0100

    added navigation drawer prototype

--- app/src/main/java/cz/uhk/umte/borikpa1/businessnews/NewsActivity.java
+++ app/src/main/java/cz/uhk/umte/borikpa1/businessnews/NewsActivity.java
@@ -1,23 +1,57 @@
 package cz.uhk.umte.borikpa1.businessnews;
 
 import android.content.Context;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.view.MenuItem;
 import android.view.View;
+
 import cz.uhk.umte.borikpa1.businessnews.adapters.NewsViewPagerAdapter;
 import cz.uhk.umte.borikpa1.businessnews.adapters.NewsItemsRecyclerViewAdapter;
 
-public class NewsActivity extends FragmentActivity {
+public class NewsActivity extends AppCompatActivity {
 
     private NewsItemsRecyclerViewAdapter adapter;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.newsCategoriesTabLayout);
         tabLayout.addTab(tabLayout.newTab().setText("Business"));
         tabLayout.addTab(tabLayout.newTab().setText("Economy"));
@@ -52,5 +86,15 @@ public class NewsActivity extends FragmentActivity {
     @Override
     public View onCreateView(String name, Context context, AttributeSet attrs) {
         return super.onCreateView(name, context, attrs);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
