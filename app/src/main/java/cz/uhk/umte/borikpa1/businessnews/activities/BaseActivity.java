3bd601dc06421e62aea5a100874285734d3dc229
pavel-borik <pavel.borik1@gmail.com>
2018-04-23 17:47:42 +0200

    fixed fragment change on rotation

--- app/src/main/java/cz/uhk/umte/borikpa1/businessnews/activities/BaseActivity.java
+++ app/src/main/java/cz/uhk/umte/borikpa1/businessnews/activities/BaseActivity.java
@@ -1,6 +1,5 @@
 package cz.uhk.umte.borikpa1.businessnews.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -40,51 +39,43 @@ public class BaseActivity extends AppCompatActivity{
         mainContent = findViewById(R.id.main_content);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.main_content, new HomeFragment())
-                .commit();
+        if( savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.main_content, new HomeFragment())
+                    .commit();
+        }
 
         navigationView.setNavigationItemSelectedListener(
-                menuItem -> {
-                    Intent intent = null;
-                    switch (menuItem.getItemId()) {
-                        case R.id.nav_home:
-//                            intent = new Intent(BaseActivity.this, HomeActivity.class);
-//                            startActivity(intent);
-//                            overridePendingTransition(0, 0);
-                            HomeFragment homeFragment = new HomeFragment();
-                            getSupportFragmentManager()
-                                    .beginTransaction()
-                                    .replace(R.id.main_content, homeFragment)
-                                    .commit();
-                            break;
-                        case R.id.nav_news:
-//                            intent = new Intent(BaseActivity.this, NewsActivity.class);
-//                            startActivity(intent);
-//                            overridePendingTransition(0, 0);
-                            ParentNewsFragment parentNewsFragment = new ParentNewsFragment();
-                            getSupportFragmentManager()
-                                    .beginTransaction()
-                                    .replace(R.id.main_content, parentNewsFragment)
-                                    .commit();
-                            break;
+            menuItem -> {
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_content, homeFragment)
+                            .commit();
+                        break;
+                    case R.id.nav_news:
+                        ParentNewsFragment parentNewsFragment = new ParentNewsFragment();
+                        getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_content, parentNewsFragment)
+                            .commit();
+                        break;
 
-                        case R.id.nav_stocks:
-//                            intent = new Intent(BaseActivity.this, NewsActivity.class);
-//                            startActivity(intent);
-//                            overridePendingTransition(0, 0);
-                            StockDataFragment stockDataFragment = new StockDataFragment();
-                            getSupportFragmentManager()
-                                    .beginTransaction()
-                                    .replace(R.id.main_content, stockDataFragment)
-                                    .commit();
-                            break;
-                    }
-                    menuItem.setChecked(true);
-                    mDrawerLayout.closeDrawers();
-                    return true;
-                });
+                    case R.id.nav_stocks:
+                        StockDataFragment stockDataFragment = new StockDataFragment();
+                        getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_content, stockDataFragment)
+                            .commit();
+                        break;
+                }
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                return true;
+            });
     }
 
     @Override
